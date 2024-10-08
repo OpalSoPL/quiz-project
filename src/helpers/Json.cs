9615faca4dc5a38db53f760c0de147c10a887a84
OpalSoPL <90208316+OpalSoPL@users.noreply.github.com>
2024-10-08 22:51:59 +0200

    feat: JSON deserializer

---
+++ src/helpers/Json.cs
@@ -0,0 +1,27 @@
+using System;
+using System.IO;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace QuizGame.Helpers;
+public static class Json
+{
+    public static async Task<T> LoadToFile <T>(string path)
+    {
+        try
+        {
+            await using FileStream stream = new FileStream(path,FileMode.Open);
+            return await JsonSerializer.DeserializeAsync<T>(stream);
+        }
+        catch(Exception e) when (
+            e is FileNotFoundException or
+            DirectoryNotFoundException or
+            UnauthorizedAccessException or
+            IOException or
+            JsonException
+        )
+        {
+            return default;
+        }
+    }
+}
