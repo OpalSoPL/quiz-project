168a21a686607b0a0bce772003b82047147ad3fb
OpalSoPL <90208316+OpalSoPL@users.noreply.github.com>
2024-10-09 19:06:14 +0200

    refactor: replace `Answer[]` with `List<Answer>` and make class `Quiz` static

--- src/class/Quiz.cs
+++ src/class/Quiz.cs
@@ -1,32 +1,28 @@
 using System;
+using System.Collections.Generic;
 using Godot;
 
-public class Quiz //todo after testing, make this class static
+public static class Quiz
 {
-    public static Question[] Questions = new Question[999];
-
-
-    public Quiz ()
-    {
-        Question question = new()
-        {
-            correctAnswer = EAnswerField.C
-        };
-        Questions[0] = question;
-    }
+    public static List<Answer> Questions = new List<Answer>();
 
     public static bool ShowResults(EAnswerField answer, uint currentQuestion)
     {
         GD.Print(answer,currentQuestion);
-        if (Answer.CheckAnswer(answer,currentQuestion))
+        if (Answers.CheckAnswer(answer,currentQuestion))
         {
             QuizScreen.Question.Text = $"answer.{answer}";
             //todo trigger correct answer particles
             return true;
         }
 
-        QuizScreen.Question.Text = $"answer.{Answer.getCorrectAnswer(currentQuestion)}";
+        QuizScreen.Question.Text = $"answer.{Answers.getCorrectAnswer(currentQuestion)}";
         //todo trigger wrong answer particles
         return false;
+    }
+
+    public static void ClearQuestions ()
+    {
+        Questions = new List<Answer>();
     }
 }
