a6338b238f2d5c3eb38646fb7265fae335c75318
OpalSoPL <90208316+OpalSoPL@users.noreply.github.com>
2024-10-14 14:03:52 +0200

    feat: curtain movement cycle

--- src/scene/quiz_screen/Curtain.cs
+++ src/scene/quiz_screen/Curtain.cs
@@ -3,15 +3,29 @@ using System;
 
 public partial class Curtain : CharacterBody2D
 {
+    private int _waitTime = 0;
+    private bool doNotCycle = false;
     public override void _PhysicsProcess(double delta)
     {
         MoveAndSlide();
+
     }
 
     public void Cycle (int wait_time)
     {
+        _waitTime = wait_time;
         Velocity=new Vector2(0,100);
-        //wait time here
+        doNotCycle = false;
+    }
+
+    public async void FloorCollision(Node2D body)
+    {
+        if (doNotCycle)
+        {
+            return;
+        }
+        await ToSignal(GetTree().CreateTimer(_waitTime), SceneTreeTimer.SignalName.Timeout);
         Velocity=new Vector2(0,-100);
+        doNotCycle = true;
     }
 }
