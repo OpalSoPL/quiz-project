6663f6b93ca492aa303e10b816d7eac3a5365ee3
OpalSoPL <90208316+OpalSoPL@users.noreply.github.com>
2024-10-18 22:37:42 +0200

    feat: default `appSettings.json` file

--- src/class/Autoload.cs
+++ src/class/Autoload.cs
@@ -7,16 +7,42 @@ public partial class Autoload : Node
 
     public override async void _EnterTree()
     {
-        SettingsDeserialize settings = await QuizGame.Helpers.Json.LoadToFile<SettingsDeserialize>(
+        SettingsDeserialize settings = await QuizGame.Helpers.Json.LoadFromFile<SettingsDeserialize>(
             (!OS.HasFeature("editor") ? OS.GetExecutablePath().GetBaseDir() : "res:/"
-                )+ "/settings.json"
+                )+ "/appSettings.json"
             );
+
+        //generate default settings file if currently is missing
+        if (settings is null && !OS.HasFeature("editor"))
+        {
+            SettingsDeserialize settingsDefault = new()
+            {
+                Version = 1,
+                Question_file = "path/to/question/file.json",
+                Run_from_question = -1
+            };
+
+            _ = QuizGame.Helpers.Json.SaveToFile(
+                    settingsDefault,
+                    $"{OS.GetExecutablePath().GetBaseDir()}/appSettings.json",
+                    true
+                );
+
+            OS.Alert("Settings file not found\nNew file has been generated. Please configure it", "Settings file not found");
+            GetTree().Quit();
+        }
+        else if (settings is null && OS.HasFeature("editor"))
+        {
+            OS.Alert("Settings file not found", "Settings file not found");
+            GD.PrintErr((!OS.HasFeature("editor") ? OS.GetExecutablePath().GetBaseDir() : "res:/")+ "/appSettings.json");
+            GetTree().Quit();
+        }
 
         Global.Debug = settings.Debug;
         Global.QuestionFile = settings.Question_file;
         Global.GoToQuestion = settings.Run_from_question;
 
-        QuestionsDeserialize QuestionFile = await QuizGame.Helpers.Json.LoadToFile<QuestionsDeserialize>(Global.QuestionFile);
+        QuestionsDeserialize QuestionFile = await QuizGame.Helpers.Json.LoadFromFile<QuestionsDeserialize>(Global.QuestionFile);
 
         Quiz.ClearQuestions();
         foreach (var item in QuestionFile.Data)
--- src/helpers/Json.cs
+++ src/helpers/Json.cs
@@ -7,7 +7,13 @@ using Godot;
 namespace QuizGame.Helpers;
 public static class Json
 {
-    public static async Task<T> LoadToFile <T>(string path)
+
+    private static JsonSerializerOptions options = new()
+    {
+        WriteIndented = true
+    };
+
+    public static async Task<T> LoadFromFile <T>(string path)
     {
         path = ProjectSettings.GlobalizePath(path);
         try
@@ -25,5 +31,35 @@ public static class Json
         {
             return default;
         }
+    }
+
+    public static async Task<bool> SaveToFile <T> (T data, string savePath, bool pretty)
+    {
+
+        if (options.WriteIndented != pretty)
+        {
+            options = new()
+            {
+                WriteIndented = pretty
+            };
+        }
+
+        try
+        {
+            await using FileStream stream = new FileStream(savePath,FileMode.OpenOrCreate);
+            await JsonSerializer.SerializeAsync(stream, data, options);
+        }
+        catch (Exception e) when (
+            e is FileNotFoundException
+            or UnauthorizedAccessException
+            or IOException
+            or DirectoryNotFoundException
+            or PathTooLongException
+        )
+        {
+            return false;
+        }
+
+        return true;
     }
 }
